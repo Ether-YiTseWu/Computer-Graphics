add9539979d4639129e7db816146df71e978a2cd
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-12-24 18:22:37 +0800

    Header

---
+++ Project04_Lighting & Shading/function.h
@@ -0,0 +1,11 @@
+#pragma once
+void make_projection(int);
+void make_view(int);
+
+void myinit();
+void draw_cube();
+void sun();
+void display();
+void display_();
+void my_reshape(int w, int h);
+void key_func(unsigned char key, int x, int y);
